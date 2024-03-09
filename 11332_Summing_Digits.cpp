0daeb166b979801dffd89237e3c3216cefe82e03
Md. Sadekur Rahman Sahad <sahad.cse8.bu@gmail.com>
2024-03-09 20:11:50 +0600

    7MarchProblem

--- 11332_Summing_Digits.cpp
+++ 11332_Summing_Digits.cpp
@@ -1,21 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     long long int n;
     int rem, sum;
-    while(cin>> n && n){
-        function:
-        sum=0;
-        while(n!=0){
-            rem=n%10;
-            sum=sum+rem;
-            n=n/10;
+    while (1){
+        cin >> n;
+        if (n == 0)    break;
+    function:
+        sum = 0;
+        while (n != 0){
+            rem = n % 10;
+            sum = sum + rem;
+            n = n / 10;
         }
-        if(sum<10) cout<<sum<< endl;
-        else {
-            n=sum;
+        if (sum < 10)  cout << sum << endl;
+        else{
+            n = sum;
             goto function;
         }
     }
